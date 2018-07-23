f3d3c8b1ec950879c3729dba8f9849e460db4fa2
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-23 19:13:54 +0300

    prepare new routes and update the current ones

--- src/router.js
+++ src/router.js
@@ -1,21 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Profile from './views/Profile.vue';
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
+    routes: [
+        { path: '/', name: 'home', component: Home },
+        { path: '/about', name: 'about', component: About },
+        { path: '/profile', name: 'profile', component: Profile }
+    ]
 })
