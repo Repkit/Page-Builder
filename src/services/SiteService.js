ae2893e3d7e3e8836e25e7f0e804f3cb59307b68
Matan <matancoder114@gmail.com>
2018-07-24 13:30:00 +0300

    SiteApp component has been added

--- src/services/SiteService.js
+++ src/services/SiteService.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 //     ? '/site'
 //     : '//localhost:3000/site'
 
-const BASE_URL = 'mongodb://page_builder_user:page_builder43@ds145921.mlab.com:45921/page_builder/site';
+// const BASE_URL = 'mongodb://page_builder_user:page_builder43@ds145921.mlab.com:45921/page_builder/site';
+const BASE_URL = '//localhost:3000/site';
 
 function query() {
     return axios.get(BASE_URL)
