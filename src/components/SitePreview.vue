66b53c437a53c0e4480df2535b1dfc590ce7a680
Matan <matancoder114@gmail.com>
2018-07-24 16:53:55 +0300

    SitePreview added

---
+++ src/components/SitePreview.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="site-preview">
+        Hello this is a preview,
+        <h1>name:{{site.name}}</h1>
+        <img :src="site.thumb">
+        {{site}}
+
+    </section>
+</template>
+
+<script>
+export default {
+  props: ["site"],
+  name: "site-nav"
+};
+</script>
--- src/views/SiteApp.vue
+++ src/views/SiteApp.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="site-app">
         welcome to site app
-        {{sites}}
+       <div v-if="sites" v-for="site in sites" :key="site._id" class="site-container">
+           <site-preview :site="site"></site-preview>
+       </div>
     </div>
 </template>
 
 <script>
+import SitePreview from "@/components/SitePreview.vue";
 export default {
   name: "site-app",
   data() {
@@ -14,14 +17,25 @@ export default {
     };
   },
   created() {
-    this.$store
-      .dispatch({ type: "loadSites" })
-      .then(sites =>{this.sites = sites});
+    this.$store.dispatch({ type: "loadSites" }).then(sites => {
+      this.sites = sites;
+    });
   },
   computed: {
     sitesToDisplay() {
       return this.$store.getters.sitesToDisplay;
     }
+  },
+  components: {
+    SitePreview
   }
 };
 </script>
+
+<style scoped>
+.site-container{
+margin: 10px;
+padding:25px;
+}
+</style>
+
