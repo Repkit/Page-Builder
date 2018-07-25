3ac953fabe13e4d56458ec1da7a3aeacb0c0d162
Matan <matancoder114@gmail.com>
2018-07-25 18:22:43 +0300

    Add Acordion group

---
+++ src/components/AcordionWidget.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <section class="accordion" @click="onClickHandler">
+        <section class="accordion-container">
+            <div name="title">{{item.name}}</div>
+            <div name="content" v-if="item.isShown">{{item.content}}</div>
+        </section>        
+    </section>        
+</template>
+
+
+<script>
+export default {
+    props:['item'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+        onClickHandler() {
+            this.$emit('clicked',this.item.id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.accordion-container{
+    text-align: center;
+}
+
+</style>
---
+++ src/components/widgets/AcordionGroupWidget.vue
@@ -0,0 +1,55 @@
+
+
+<template>
+    <!-- <div class="widget button-widget" :style="element.styles"> -->
+    <div class="widget acordion-group-widget">
+        <section class="accordion-group" v-for="item in items" :key="item.id">
+            <accordion @clicked="toggleIsShown" :item="item">
+            </accordion>
+        </section>
+
+
+    </div>
+</template>
+
+<script>
+import accordion from '@/components/AcordionWidget.vue'
+
+export default {
+  name: "acordion-group-widget",
+  props: ["element"],
+  methods: {
+    toggleIsShown(itemId){
+       var child= this.items.find(item =>item.id === itemId)
+       if(child)child.isShown= !child.isShown
+
+
+    }
+  },
+  data() {
+    return {
+      items: [
+        { id:'sdsd',name: "Gal", content: 27 ,isShown:false},
+        { id:'qdsc',name: "Idan", content: 28 ,isShown:false},
+        { id:'sdsw',name: "Ofir", content: 24 ,isShown:false}
+      ],
+      accordions: []
+    };
+  },
+  components: {
+    accordion
+  }
+};
+</script>
+
+<style scoped>
+.acordion-group-widget{
+    width: 36vw;
+    margin: 0 auto;
+}
+.accordion-group{
+    cursor: pointer;
+    border: 1px solid black;
+}
+</style>
+
