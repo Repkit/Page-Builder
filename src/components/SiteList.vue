d98b0f76ad032b7cfac05b3e057918a137d80d3b
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-26 00:10:46 +0300

    Merge the site-preview component to into the site list

--- src/components/SiteList.vue
+++ src/components/SiteList.vue
@@ -1,27 +1,38 @@
 <template>
     <div class="site-list">
 
-        <div v-if="sites" v-for="site in sites" :key="site._id">
-            <site-preview :site="site"></site-preview>
+        <div v-if="sites" v-for="site in sites" :key="site._id" class="site-list-item">
+            <h3><router-link :to="'/'+site._id">{{site.name}}</router-link></h3>
+            <router-link :to="'/'+site._id"><img :src="site.thumb"></router-link>
         </div>
 
     </div>
 </template>
 
 <script>
-import SitePreview from "@/components/SitePreview.vue";
-
 export default {
     name: 'site-list',
     props: [ 'sites' ],
-    components: { SitePreview },
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 .site-list {
-    clear: both;
     display: flex;
     flex-wrap: wrap;
+
+    .site-list-item {
+        margin: 10px;
+        padding: 25px;
+        align-items: center;
+        border: 2px solid #00000014;
+        width: 300px;
+
+        h3 {
+            text-align: center;
+            display: block;
+            margin-top: 0;
+        }
+    }
 }
 </style>
