13dd1b4bbd23fef5d93b91029cbcc6e853a79b12
Matan <matancoder114@gmail.com>
2018-07-26 09:44:30 +0300

    Enter to profile afther sucssefull login

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -13,32 +13,34 @@
 
 <script>
 export default {
-    name: 'user-login',
-    data() {
-        return {
-            user: {
-                userName: '',
-                password: ''
-            },
-            isWorngCreds: false
-        };
+  name: "user-login",
+  data() {
+    return {
+      user: {
+        userName: "",
+        password: ""
+      },
+      isWorngCreds: false
+    };
+  },
+  computed: {
+    isUserLogin() {
+      return this.$store.getters.isUserLoggedIn;
     },
-    computed: {
-        isUserLogin() {
-            return this.$store.getters.isUserLoggedIn;
-        },
-        loggedInUser() {
-            return this.$store.getters.loggedInUser.userName;
-        }
-    },
-    methods: {
-        login() {
-            let user = { userName: this.user.userName, password: this.user.password };
-            this.$store.dispatch({ type: 'login', user })
-                .then(user => {
-                    this.isWorngCreds = (user) ? false : true;
-                });
-        }
+    loggedInUser() {
+      return this.$store.getters.loggedInUser.userName;
     }
+  },
+  methods: {
+    login() {
+      let user = { userName: this.user.userName, password: this.user.password };
+      this.$store.dispatch({ type: "login", user }).then(user => {
+        if (user) {
+          this.isWorngCreds = false;
+          this.$router.push(`/profile`);
+        } else this.isWorngCreds = true;
+      });
+    }
+  }
 };
 </script>
