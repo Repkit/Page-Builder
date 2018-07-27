796499415443a6ca5c1775e06f32ab61f2bfffe2
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-27 10:18:03 +0300

    First steps with editor screens

---
+++ src/components/editor/screens/ElementsList.vue
@@ -0,0 +1,16 @@
+<template>
+    <aside class="site-elements-list">
+
+        <h1>Elements List</h1>
+
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'site-elements-list',
+    props: [ 'site', 'selected' ],
+};
+
+// Search Using: https://vuejs.org/v2/guide/transitions.html#Staggering-List-Transitions
+</script>
---
+++ src/components/editor/screens/ElementsTree.vue
@@ -0,0 +1,14 @@
+<template>
+    <aside class="site-elements-tree">
+
+        <h1>Elements Tree</h1>
+
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'site-elements-tree',
+    props: [ 'site', 'selected' ],
+};
+</script>
