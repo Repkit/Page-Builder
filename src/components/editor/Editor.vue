25962a958d2743eb1a488601d468c137868460b7
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-27 15:42:36 +0300

    Add elements list to the editor

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -9,8 +9,13 @@
             <p class="text-center flex-grow"> <i>&#10515;</i> <br> Tree </p>
         </div>
 
-        <div class="new-section">
+        <div class="add-new-section">
             <h2 @click="$emit('add-element', 'section')">Add New Section</h2>
+        </div>
+
+        <div class="add-new-elements">
+            <h2>Elements List</h2>
+            <elements-list></elements-list>
         </div>
 
         <div class="selected-element" v-if="selectedElement">
@@ -25,13 +30,17 @@
 
 <script>
 import EditorService from '@/services/EditorService.js';
+import ElementsList from '@/components/editor/screens/ElementsList.vue';
 
 export default {
     name: 'editor',
     props: [ 'site', 'selected' ],
+    components: { ElementsList },
     computed: {
         selectedElement() {
-            return (this.site && this.site._id) ? EditorService.getSelectedElementById(this.site, this.selected) : null;
+            return (this.site && this.site._id)
+                ? EditorService.getSelectedElementById(this.site, this.selected)
+                : null;
         }
     }
 };
@@ -39,74 +48,77 @@ export default {
 
 <style scoped lang="scss">
 .editor {
-    background-color: #e6e9ec;
-    color: #6d7882;
-    height: 100vh;
+  background-color: #e6e9ec;
+  color: #6d7882;
+  height: 100vh;
 
-    h1 {
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 1.25em;
+    line-height: 1.5;
+    text-align: center;
+    padding-top: 0.25em;
+    padding-bottom: 0.5em;
+    background-color: #4e4c4d;
+    color: #fff;
+  }
+
+  h2 {
+    margin: 0;
+    padding: 0;
+    font-size: 0.85em;
+    line-height: 1.1;
+    text-align: center;
+  }
+
+  > div {
+    padding-top: 1em;
+    padding-bottom: 1em;
+    text-align: center;
+    background-color: #fff;
+    margin-bottom: 10px;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+
+    &.elements-navigation {
+      padding: 0;
+      cursor: pointer;
+
+      p {
         margin: 0;
-        padding: 0;
-        font-size: 1.25em;
-        line-height: 1.5;
-        text-align: center;
-        padding-top: 0.25em;
-        padding-bottom: 0.5em;
-        background-color: #4e4c4d;
-        color: #fff;
-    }
-
-    h2 {
-        margin: 0;
-        padding: 0;
-        font-size: 0.85em;
-        line-height: 1.1;
-        text-align: center;
-    }
-
-    > div {
         padding-top: 1em;
         padding-bottom: 1em;
-        text-align: center;
-        background-color: #fff;
-        margin-bottom: 10px;
-        box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+        font-size: 0.75em;
 
-        &.elements-navigation {
-            padding: 0;
-            cursor: pointer;
-
-            p {
-                margin: 0;
-                padding-top: 1em;
-                padding-bottom: 1em;
-                font-size: 0.75em;
-
-                i {
-                    display: inline-block;
-                    font-size: 2em;
-                    padding-bottom: 0.25em;
-                    font-style: normal;
-                }
-
-                &:hover {
-                    background-color: #eee;
-                }
-            }
+        i {
+          display: inline-block;
+          font-size: 2em;
+          padding-bottom: 0.25em;
+          font-style: normal;
         }
 
-        &.new-section {
-            cursor: pointer;
-
-            &:hover {
-                background-color: #eee;
-            }
+        &:hover {
+          background-color: #eee;
         }
-
-        &.selected-element {
-            p {
-                text-align: left;
-            }
-        }
+      }
     }
+
+    &.add-new-section {
+      cursor: pointer;
+
+      &:hover {
+        background-color: #eee;
+      }
+    }
+
+    &.add-new-elements {
+    }
+
+    &.selected-element {
+      p {
+        text-align: left;
+      }
+    }
+  }
 }
 </style>
--- src/components/editor/screens/ElementsList.vue
+++ src/components/editor/screens/ElementsList.vue
@@ -1,16 +1,72 @@
 <template>
-    <aside class="site-elements-list">
+    <aside class="elements-list">
 
-        <h1>Elements List</h1>
+        <input type="search" v-model="filterBy" placeholder="Search elements..." />
+
+        <div class="flex flex-wrap">
+
+            <div v-for="element in elementsToDisplay" :key="element.type"
+                class="element-type flex-grow" draggable="true">
+                XX<br><br>
+                {{element.label}}
+            </div>
+
+        </div>
 
     </aside>
 </template>
 
 <script>
 export default {
-    name: 'site-elements-list',
-    props: [ 'site', 'selected' ],
+    name: 'elements-list',
+    data() {
+        return {
+            elements: [
+                { type: 'section', label: 'Section', icon: '' },
+                { type: 'columns', label: 'Columns', icon: '' },
+                { type: 'text', label: 'Text', icon: '' },
+                { type: 'list', label: 'List', icon: '' },
+                { type: 'button', label: 'Button', icon: '' },
+                { type: 'divider', label: 'Divider', icon: '' },
+                { type: 'image', label: 'Image', icon: '' },
+                { type: 'video', label: 'Video', icon: '' },
+                { type: 'map', label: 'Map', icon: '' },
+                { type: 'counter', label: 'Counter', icon: '' },
+                { type: 'progressBar', label: 'ProgressBar', icon: '' },
+                { type: 'accordion', label: 'Accordion', icon: '' },
+            ],
+            filterBy: ''
+        }
+    },
+    computed: {
+        elementsToDisplay() {
+            return this.elements.filter( element => element.label.toLowerCase().includes(this.filterBy.toLowerCase()) )
+        }
+    }
 };
 
 // Search Using: https://vuejs.org/v2/guide/transitions.html#Staggering-List-Transitions
 </script>
+
+<style scoped lang="scss">
+input {
+    width: 96%;
+    max-width: 100%;
+    margin: 0.5em 2% 0;
+    padding: 5px 10px;
+    border: 1px solid #999;
+}
+.element-type {
+    flex: 0 46%;
+    margin: 10px 2%;
+    padding: 10px 5px;
+    border-radius: 3px;
+    border: 1px solid #e3e3e3;
+    background-color: #eee;
+    cursor: move;
+
+    &:hover {
+        box-shadow: 0 2px 3px rgba(0,0,0,.2);
+    }
+}
+</style>
