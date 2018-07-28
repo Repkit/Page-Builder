c96eccb18ce4aea6c454c208f306d4a7a31721a4
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-29 00:05:58 +0300

    Update editor store

--- src/modules/EditorModule.js
+++ src/modules/EditorModule.js
@@ -2,38 +2,54 @@ import EditorService from '@/services/EditorService';
 
 export default {
     state: {
+        // Site Data
         site: {},
+
+        // Editor
         editMode: false,
-        editorScreen:'Element',
+        editorScreen: 'Element',
+
+        // Selected Element Data
         selectedElement: null,
-        selectedElementId: null,
     },
     getters: {
+        // Site Data
+        site: state => state.site,
+
+        // Editor
         editMode: state => state.editMode,
+        editorScreen: state => state.editorScreen,
+
+        // Selected Element Data
         selectedElement: state => state.selectedElement,
-        selectedElementId: state => state.selectedElement,
-        editorScreen:state => state.editorScreen,
     },
     mutations: {
-        setEditorScreen(state,{status}){
-          state.editorScreen=status
-        },
+        // Site Data
         setSiteToEdit(state, { site }) {
             state.site = site;
         },
+        removeElement(state, { id }) {
+            console.log('id of the selected element:', id);
+            console.log('elements:', state.site.elements);
+            console.log('elements:', EditorService.removeSelectedElementById(state.site.elements, id));
+        },
+        cloneElement(state, { id, element }) {
+        },
+        editElement(state, { id, element }) {
+        },
+
+        // Editor
+        setEditorScreen(state, { status }) {
+            state.editorScreen = status;
+        },
+
+        // Selected Element Data
         updateSelectedElement(state, { id }) {
-            state.selectedElementId = id;
             state.selectedElement = EditorService.getSelectedElementById(state.site, id);
         },
         removeSelectedElement(state) {
-            state.selectedElementId = null;
             state.selectedElement = null;
-        },
-        removeElement(state, { id }){
-            console.log('id of the selected element:',id)
-            console.log('elements:',state.site.elements)
-            console.log('elements:',EditorService.removeSelectedElementById(state.site.elements, id))
-
         }
-    },
+
+    }
 }
