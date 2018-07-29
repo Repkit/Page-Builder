003b0018d17fe0c3159322c61036f2ca9dd3ff61
Shuvy <shuvyan@gmail.com>
2018-07-29 17:07:59 +0300

    Add element edit progress bar and image

--- src/components/editor/screens/EditorEditElement.vue
+++ src/components/editor/screens/EditorEditElement.vue
@@ -18,7 +18,7 @@ import ImageElement from '@/components/elements/editor/ImageElement.vue';
 import ListElement from '@/components/elements/editor/ListElement.vue';
 // import MapElement from '@/components/elements/editor/MapElement.vue';
 import ButtonElement from '@/components/elements/editor/ButtonElement.vue';
-// import ProgressBarElement from '@/components/elements/editor/ProgressBarElement.vue';
+import ProgressBarElement from '@/components/elements/editor/ProgressBarElement.vue';
 // import AcordionGroupElement from '@/components/elements/editor/AcordionGroupElement.vue';
 // import CounterElement from '@/components/elements/editor/CounterElement.vue';
 
@@ -43,7 +43,7 @@ export default {
         ListElement,
         // MapElement,
         ButtonElement,
-        // ProgressBarElement,
+        ProgressBarElement,
         // AcordionGroupElement,
         // CounterElement,
     }
---
+++ src/components/elements/editor/ProgressBarElement.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="element-editor progress-bar-element-editor">
+
+        <h3>Content</h3>
+        <form-fields :fields="content" />
+
+        <h3>Style</h3>
+        <form-fields :fields="styles" />
+
+    </div>
+</template>
+
+<script>
+import FormFields from '@/components/controls/FormFields.vue';
+
+export default {
+    name: 'progress-bar-element-editor',
+    props: [ 'element' ],
+    components: { FormFields },
+    data() {
+        return {
+            content: [
+                {
+                    cmp: 'input-text',
+                    name: 'text',
+                    label: 'Text',
+                    placeholder: 'Enter some text...',
+                    value: this.element.data.text
+                },
+                {
+                    cmp: 'input-range',
+                    name: 'min',
+                    label: 'min',
+                    value: this.element.data.min
+                },
+                 {
+                    cmp: 'input-range',
+                    name: 'max',
+                    label: 'max',
+                    value: this.element.data.max
+                },  
+                {
+                    cmp: 'input-range',
+                    name: 'value',
+                    label: 'value',
+                    value: this.element.data.value
+                },
+                   {
+                    cmp: 'input-range',
+                    name: 'step',
+                    label: 'step',
+                    value: this.element.data.step
+                },   
+                    {
+                    cmp: 'input-range',
+                    name: 'interval',
+                    label: 'interval',
+                    value: this.element.data.interval
+                },   
+            ],
+            styles: [
+                {
+                    cmp: 'input-range',
+                    name: 'fontSize',
+                    label: 'Font Size',
+                    value: this.element.styles.fontSize
+                }
+            ]
+        }
+    }
+};
+</script>
