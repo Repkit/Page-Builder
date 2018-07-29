193581a5f3b46d2159911acc8f1769edefddf500
Shuvy <shuvyan@gmail.com>
2018-07-29 12:35:12 +0300

    Add button element edit cmp

---
+++ src/components/elements/editor/ButtonElement.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="element-editor button-element-editor">
+
+        <h3>Content</h3>
+
+        <label>Text</label>
+        <input v-model="element.data.text" type="text" name="text" placeholder="Enter some text..." />
+
+        <label>Link URL</label>
+        <input v-model="element.data.link" type="url" name="link" placeholder="https://your-link.com" />
+
+        <h3>Style</h3>
+
+        <label>Font Size</label>
+        <input v-model="element.style.fontSize" type="range" name="fontSize" />
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'button-element-editor',
+    props: [ 'element' ],
+};
+</script>
