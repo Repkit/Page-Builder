93df4f564c6ed86941b2f1f632e3359ea1c3ff9c
Shuvy <shuvyan@gmail.com>
2018-07-29 12:19:50 +0300

    Add list element editor

---
+++ src/components/elements/editor/ListElement.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="element-editor list-element-editor">
+
+        <h3>Content</h3>
+
+        <ul>
+            <li v-for="(item, index) in items" :key="item.id">
+                <label>{{index}}.</label>
+                <input v-model="items[index]" type="text" placeholder="index">
+                <button @click="delItem(index)">-</button>
+            </li>
+        </ul>
+
+
+        <button @click="addItem">Add item</button>
+
+        <button @click="saveItems">save items</button>
+
+
+
+        <h3>Style</h3>
+
+        <label>Font Size</label>
+        <input v-model="element.style.fontSize" type="range" name="fontSize" />
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "list-element-editor",
+  props: ["element"],
+  data() {
+    return {
+      items: [3]
+    };
+  },
+  methodes: {
+    saveItems() {
+      this.element.data.text = this.items;
+    },
+    delItem(index){
+        this.items.splice(1,index);
+    },
+    addItem(){
+        this.items.map((item)=> ++item);
+    }
+  }
+};
+</script>
