6b6e3c3f3e9fac0a317f93dda80e23db1d5365ce
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-30 19:54:30 +0300

    Fix controls default values

--- src/components/controls/fields/InputUrlField.vue
+++ src/components/controls/fields/InputUrlField.vue
@@ -20,7 +20,7 @@ export default {
     computed: {
         value: {
             get() {
-                return parseInt(this.newField.value) || this.newField.default || '';
+                return this.newField.value || this.newField.default || '';
             },
             set(newVal) {
                 this.newField.value = newVal;
