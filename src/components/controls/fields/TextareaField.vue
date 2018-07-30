1b2eb484728f8ff428fc960a6c8e7445f6af72ac
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-30 19:54:53 +0300

    Add default value to text area

--- src/components/controls/fields/TextareaField.vue
+++ src/components/controls/fields/TextareaField.vue
@@ -2,7 +2,7 @@
     <div class="textarea-field" v-if="newField">
 
         <label>{{newField.label}}</label>
-        <textarea v-model="newField.value" @input="$emit('change', newField, idx)"
+        <textarea v-model="value" @input="$emit('change', newField, idx)"
             :rows="newField.rows" :placeholder="newField.placeholder" />
 
     </div>
@@ -15,6 +15,16 @@ export default {
     data() {
         return {
             newField: Object.assign( this.field )
+        }
+    },
+    computed: {
+        value: {
+            get() {
+                return this.newField.value || this.newField.default || '';
+            },
+            set(newVal) {
+                this.newField.value = newVal;
+            }
         }
     }
 };
