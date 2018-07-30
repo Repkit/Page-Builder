82b25acb635a9beb4d376830bb446f9f6084ae76
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-30 14:39:05 +0300

    Build the list element editor form

--- src/components/elements/editor/ListElement.vue
+++ src/components/elements/editor/ListElement.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="element-editor list-element-editor">
 
-        
-         <h3>Content</h3>
-        <form-fields :fields="content" />
+        <div class="editor-box">
+            <h2>Content</h2>
+            <form-fields class="content" :fields="content" />
+        </div>
 
-        <button @click="addItem">Add item</button>
-
-        <button @click="saveItems">save items</button>
-
-       <h3>Style</h3>
-       <form-fields :fields="styles" />
+        <div class="editor-box">
+            <h2>Style</h2>
+            <form-fields class="content" :fields="styles" />
+        </div>
 
     </div>
 </template>
@@ -19,20 +18,16 @@
 import FormFields from '@/components/controls/FormFields.vue';
 
 export default {
-  name: "list-element-editor",
-  props: ["element"],
-  components: { FormFields },
-  created() {
-         this.items = this.element.data.text;
-  },
-  data() {
-    return {
-      items: null,
-      content: [
+    name: 'list-element-editor',
+    props: [ 'element' ],
+    components: { FormFields },
+    data() {
+        return {
+            content: [
                 {
-                    cmp: 'input-text',
+                    cmp: 'input-text-repeater',
                     name: 'text',
-                    label: 'Text',
+                    label: 'List',
                     placeholder: 'Enter some text...',
                     value: this.element.data.text
                 },
@@ -45,20 +40,7 @@ export default {
                     value: this.element.styles.fontSize
                 }
             ]
-    };
-  },
-  methods: {
-    saveItems() {
-      this.element.data.text = this.items;
-    },
-    delItem(index){
-        this.items.splice(1,index);
-    },
-    addItem(){
-        // 2 ways todo check
-        this.items.map((item)=> ++item);
-        //  this.items.push( this.items[this.items.length - 1] + 1);
+        };
     }
-  }
 };
 </script>
