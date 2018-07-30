7d5f80e0d21a18a5615f39fb2e770eee3385e4b1
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-30 18:44:15 +0300

    Update text element form fields

--- src/components/elements/editor/TextElement.vue
+++ src/components/elements/editor/TextElement.vue
@@ -44,7 +44,35 @@ export default {
                     cmp: 'input-range',
                     name: 'fontSize',
                     label: 'Font Size',
+                    unit: 'px',
+                    default: '30',
                     value: this.element.styles.fontSize
+                },
+                {
+                    cmp: 'input-range',
+                    name: 'letterSpacing',
+                    label: 'Letter Spacing',
+                    unit: 'px',
+                    value: this.element.styles.letterSpacing
+                },
+                {
+                    cmp: 'select',
+                    name: 'fontWeight',
+                    options: [
+                        { label: 'Normal', value: 'normal' },
+                        { label: 'Bold', value: 'bold' },
+                        { label: '100', value: '100' },
+                        { label: '200', value: '200' },
+                        { label: '300', value: '300' },
+                        { label: '400', value: '400' },
+                        { label: '500', value: '500' },
+                        { label: '600', value: '600' },
+                        { label: '700', value: '700' },
+                        { label: '800', value: '800' },
+                        { label: '900', value: '900' },
+                    ],
+                    label: 'Font Weight',
+                    value: this.element.styles.fontWeight
                 },
                 {
                     cmp: 'select',
@@ -54,8 +82,8 @@ export default {
                         { label: 'Center', value: 'center' },
                         { label: 'Right', value: 'right' },
                     ],
-                    label: 'Align',
-                    value: this.element.styles.fontSize
+                    label: 'Text Align',
+                    value: this.element.styles.textAlign
                 }
             ]
         }
