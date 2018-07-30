a32a658b8bf4e804b52609dca2fd84a4c87337c3
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-31 01:06:19 +0300

    Improve the list element

--- src/components/elements/list/ListForm.vue
+++ src/components/elements/list/ListForm.vue
@@ -34,10 +34,39 @@ export default {
             ],
             styles: [
                 {
-                    cmp: 'input-range',
                     name: 'fontSize',
+                    cmp: 'input-range',
                     label: 'Font Size',
+                    default: '18px',
+                    unit: 'px',
                     value: this.element.styles.fontSize
+                },
+                {
+                    name: 'letterSpacing',
+                    cmp: 'input-range',
+                    label: 'Letter Spacing',
+                    value: this.element.styles.letterSpacing
+                },
+                {
+                    name: 'fontWeight',
+                    cmp: 'select',
+                    label: 'Font Weight',
+                    options: [
+                        { label: 'Default', value: '' },
+                        { label: 'Normal', value: 'normal' },
+                        { label: 'Bold', value: 'bold' },
+                        { label: '100', value: '100' },
+                        { label: '200', value: '200' },
+                        { label: '300', value: '300' },
+                        { label: '400', value: '400' },
+                        { label: '500', value: '500' },
+                        { label: '600', value: '600' },
+                        { label: '700', value: '700' },
+                        { label: '800', value: '800' },
+                        { label: '900', value: '900' },
+                    ],
+                    default: '',
+                    value: this.element.styles.fontWeight
                 }
             ]
         };
