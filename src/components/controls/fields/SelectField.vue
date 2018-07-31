99fc8098a243b682ef0334e432e7bdf80e68b318
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-31 13:40:14 +0300

    Add default values support for select field controls

--- src/components/controls/fields/SelectField.vue
+++ src/components/controls/fields/SelectField.vue
@@ -2,7 +2,7 @@
     <div class="select-field flex align-baseline" v-if="newField">
 
         <label>{{newField.label}}</label>
-        <select v-model="newField.value" @change="$emit('change', newField, idx)">
+        <select v-model="value" @change="$emit('change', newField, idx)">
             <option v-for="(option, idx) in newField.options" :key="idx"
                 :value="option.value" :selected="{selected: option.value}">
                 {{option.label}}
@@ -19,6 +19,18 @@ export default {
     data() {
         return {
             newField: Object.assign( this.field )
+        }
+    },
+    computed: {
+        value: {
+            get() {
+                let val = this.newField.value || this.newField.default || '';
+                val = val.replace( this.newField.prefix, '' ).replace( this.newField.suffix, '' );
+                return val;
+            },
+            set(newVal) {
+                this.newField.value = this.newField.prefix + newVal + this.newField.suffix;
+            }
         }
     }
 };
