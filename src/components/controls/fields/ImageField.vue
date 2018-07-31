9354d1cc51ea578d3f198feedc1a9ec08e0c2d2c
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-31 11:54:05 +0300

    Image field display with preperation to upload functionality

--- src/components/controls/fields/ImageField.vue
+++ src/components/controls/fields/ImageField.vue
@@ -2,7 +2,12 @@
     <div class="image-field" v-if="newField">
 
         <label>{{newField.label}}</label>
-        <img :src="value" />
+        <div class="image-preview">
+            <img :src="value" />
+            <div class="image-upload" @click="cludinary">
+                <span> Upload </span>
+            </div>
+        </div>
         <input v-model="value" @input="$emit('change', newField, idx)"
             type="url" name="newField.name" :placeholder="newField.placeholder" />
 
@@ -27,13 +32,50 @@ export default {
                 this.newField.value = newVal;
             }
         }
+    },
+    methods: {
+        cludinary() {
+            // Do something...
+        }
     }
 };
 </script>
 
 <style lang="scss" scoped>
-img {
+.image-preview {
+    position: relative;
     margin-bottom: 5px;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+
+    img {
+        margin: 0 auto;
+    }
+
+    .image-upload {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        left: 5px;
+        bottom: 5px;
+        background-color: rgba( 0, 0, 0, 0.5 );
+        color: #fff;
+        display: none;
+        cursor: pointer;
+
+        span {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-weight: bold;
+        }
+    }
+
+    &:hover .image-upload {
+        display: block;
+    }
 }
 input {
     margin-top: 5px;
