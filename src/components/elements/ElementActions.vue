d87ca1b35420f9348e11afa6b078179679f892ee
Matan <matancoder114@gmail.com>
2018-08-01 18:38:27 +0300

    Change function name

--- src/components/elements/ElementActions.vue
+++ src/components/elements/ElementActions.vue
@@ -23,12 +23,7 @@ export default {
     },
     methods: {
         editElement() {
-            // Reset Old
-            this.$store.commit({ type: 'removeSelectedElement' });
-            this.$store.commit({ type: 'setEditorScreen', screen: '' });
-
-            // Set New
-            this.$store.commit({ type: 'updateSelectedElement', site: this.site, id: this.id });
+            this.$store.commit({ type: 'updateSelectedElementById', site: this.site, id: this.id });
             this.$store.commit({ type: 'setEditorScreen', screen: 'editor-edit-element' });
         },
         cloneElement() {
