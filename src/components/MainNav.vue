e338fcf10ba23265eaf9fbfcf4356e96e0baf0b0
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-08-01 08:38:05 +0300

    Main nav styling

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,23 +1,19 @@
 <template>
+    <div class="main-nav flex space-between align-center">
 
-    <div class="main-nav">
+        <h1>Page Builder</h1>
+        <ul class="flex-row align-center flex inline-list" arial-label="Main navigation bar">
+            <li><router-link to="/"> Home </router-link></li>
+            <li><router-link to="/about"> About </router-link></li>
+            <li><router-link to="/profile" v-if="isUserLoggedIn"> My Profile </router-link></li>
+            <li><button @click="logout" v-if="isUserLoggedIn"> Logout </button></li>
+            <li><router-link to="/signup" v-if="!isUserLoggedIn"> Signup </router-link></li>
+            <li>
+                <span @click="toggleLogin" v-if="!isUserLoggedIn"> Login </span>
+                <user-login v-if="!isUserLoggedIn && displayLogin" @toggleLogin="toggleLogin"></user-login>
+            </li>
+        </ul>
 
-        <div class="flex space-between align-center">
-
-            <h1>Page Builder</h1>
-            <ul class="flex-row align-center flex inline-list" arial-label="Main navigation bar">
-                <li><router-link to="/"> Home </router-link></li>
-                <li><router-link to="/about"> About </router-link></li>
-                <li><router-link to="/profile" v-if="isUserLoggedIn"> My Profile </router-link></li>
-                <li><button @click="logout" v-if="isUserLoggedIn"> Logout </button></li>
-                <li><router-link to="/signup" v-if="!isUserLoggedIn"> Signup </router-link></li>
-                <li>
-                    <span @click="toggleLogin" v-if="!isUserLoggedIn"> Login </span>
-                    <user-login v-if="!isUserLoggedIn && displayLogin" @toggleLogin="toggleLogin"></user-login>
-                </li>
-            </ul>
-
-        </div>
     </div>
 </template>
 
@@ -56,20 +52,23 @@ export default {
 
 <style scoped lang="scss">
 .main-nav {
-    padding: 20px 0;
     h1 {
-        padding-left: 15px;
+        margin: 0 10px;
     }
 
-    li {
-        padding: 0.5em 1em;
+    ul {
+        // margin: 0;
 
-        a {
-            font-weight: bold;
-            color: #000;
+        li {
+            padding: 0.5em 1em;
 
-            &.router-link-exact-active {
-                color: #42b983;
+            a {
+                font-weight: bold;
+                color: #000;
+
+                &.router-link-exact-active {
+                    color: #42b983;
+                }
             }
         }
     }
