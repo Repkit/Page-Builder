db5d1f4368c950dde35cf85c835c37545b8e78a4
Shuvy <shuvyan@gmail.com>
2018-08-01 09:42:29 +0300

    Add responiv nav bar

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,11 +1,12 @@
 <template>
 
     <div class="main-nav">
-
+        <div ref="toggleScreen" class="toggle-screen" @click="toggleNavMobile"></div>
         <div class="flex space-between align-center">
 
-            <h1>Page Builder</h1>
-            <ul class="flex-row align-center flex inline-list" arial-label="Main navigation bar">
+            <h1><router-link to="/">Page Builder</router-link></h1>
+
+            <ul ref="navDesktop" class="nav-desktop flex-row align-center flex inline-list" arial-label="Main navigation bar">
                 <li><router-link to="/"> Home </router-link></li>
                 <li><router-link to="/about"> About </router-link></li>
                 <li><router-link to="/profile" v-if="isUserLoggedIn"> My Profile </router-link></li>
@@ -16,7 +17,12 @@
                     <user-login v-if="!isUserLoggedIn && displayLogin" @toggleLogin="toggleLogin"></user-login>
                 </li>
             </ul>
-
+            <!-- Mobile-nav -->
+            <div ref="navMobile" class="nav-mobile" @click="toggleNavMobile">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -29,10 +35,16 @@ export default {
     components: { UserLogin },
     data() {
         return {
+            navBarMObile: false,
             displayLogin: false,
         }
     },
     methods: {
+        toggleNavMobile(){
+        this.$refs.navMobile.classList.toggle("change");
+        this.$refs.navDesktop.classList.toggle("open");
+        this.$refs.toggleScreen.classList.toggle("show-div");
+        },
         toggleLogin() {
             this.displayLogin = !this.displayLogin;
         },
@@ -59,6 +71,11 @@ export default {
     padding: 20px 0;
     h1 {
         padding-left: 15px;
+        a {
+            text-decoration: none;
+            font-weight: bold;
+            color: #000;
+        }
     }
 
     li {
@@ -67,6 +84,7 @@ export default {
         a {
             font-weight: bold;
             color: #000;
+            text-decoration: none;
 
             &.router-link-exact-active {
                 color: #42b983;
@@ -74,4 +92,98 @@ export default {
         }
     }
 }
+ .nav-mobile {
+        display: none;
+    }
+ .toggle-screen {
+    display: none;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    left: 0;
+    top: 0;
+    background: rgba(42, 42, 42, 0.6);
+    z-index: 0;
+    opacity: 0.9;
+    visibility: hidden;
+    cursor: pointer;
+    transition: opacity .5s; 
+    }
+
+
+@media (max-width: 800px) {
+    .show-div {
+            visibility: visible;
+            opacity: 1; 
+            display:block;
+          }
+    .nav-desktop{
+        z-index: 1;
+        padding-top: 20px;
+        flex-direction: column;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        right: 0;
+        background: rgba(255, 255, 255, 0.808);
+        border-left: 1px solid rgba(102, 102, 102, 0.5);
+        min-width: 200px;
+        transform: translate(100%, 0);
+        transition: all .8s;
+
+        &.open{
+            transform: translate(0, 0);
+        }
+        li a {
+        border-bottom: 2px solid black;
+        text-decoration: dotted;
+        display: block;
+        // width: 100px;
+        // height: 130px;
+        line-height: 80px;
+        text-align: center;
+        font-size: 16px;
+        }
+
+        li a:hover {
+            color: #42b983;
+        }
+    }
+
+    /* nav-mobile button */
+    
+    .nav-mobile {
+         z-index: 1;
+        cursor: pointer;
+        display: block;
+        float: right;
+        padding: 10px;
+    }
+    
+    .bar1, .bar2, .bar3 {
+        width: 35px;
+        height: 5px;
+        background-color: black;
+        margin: 6px 0;
+        transition: 0.4s;
+    }
+    
+    /* Rotate first bar */
+    .change .bar1 {
+        -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;
+        transform: rotate(-45deg) translate(-9px, 6px) ;
+    }
+    
+    /* Fade out the second bar */
+    .change .bar2 {
+        opacity: 0;
+    }
+    
+    /* Rotate last bar */
+    .change .bar3 {
+        -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
+        transform: rotate(45deg) translate(-8px, -8px) ;
+    }
+    
+    }
 </style>
