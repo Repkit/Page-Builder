e6127c2b11159ee67e37f8dc5f7b77561662bce2
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-08-03 12:06:20 +0300

    Refactor select field control

--- src/components/controls/fields/SelectField.vue
+++ src/components/controls/fields/SelectField.vue
@@ -2,7 +2,7 @@
     <div class="select-field flex align-baseline" v-if="newField">
 
         <label>{{newField.label}}</label>
-        <select v-model="value" @change="$emit('change', newField.name, newField.value, idx)">
+        <select v-model="newField.value" @change="update">
             <option v-for="(option, optIdx) in newField.options" :key="optIdx"
                 :value="option.value" :selected="{selected: option.value}">
                 {{option.label}}
@@ -18,28 +18,29 @@ export default {
     props: [ 'field', 'idx' ],
     data() {
         return {
-            newField: JSON.parse(JSON.stringify(this.field))
+            newField: null
         }
     },
-    computed: {
-        value: {
-            get() {
-                let val = this.newField.value || this.newField.default || '';
-                val = val.replace( this.newField.prefix, '' ).replace( this.newField.suffix, '' );
-                return val;
-            },
-            set(newVal) {
-                let prefix = ( this.newField.prefix ) ? this.newField.prefix : '';
-                let suffix = ( this.newField.suffix ) ? this.newField.suffix : '';
-                this.newField.value = prefix + newVal + suffix;
-            }
+    methods: {
+        update() {
+            let value = this.newField.value;
+            let prefix = ( this.newField.prefix ) ? this.newField.prefix : '';
+            let suffix = ( this.newField.suffix ) ? this.newField.suffix : '';
+            this.newField.value = prefix + value + suffix;
+            this.$emit('change', this.newField.name, this.newField.value, this.idx);
         }
     },
     watch: {
         field: {
-            deep: true,
+            immediate: true,
             handler() {
-                this.newField = JSON.parse(JSON.stringify(this.field))
+                this.newField = JSON.parse(JSON.stringify(this.field));
+                let value = this.newField.value || this.newField.default || '';
+                let prefix = ( this.newField.prefix ) ? this.newField.prefix : '';
+                let suffix = ( this.newField.suffix ) ? this.newField.suffix : '';
+                value = value.replace( prefix, '' );
+                value = value.replace( suffix, '' );
+                this.newField.value = value;
             }
         }
     }
