8b5c034b6d722e9a2782fb641686f77fbeaa2960
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-08-03 12:04:07 +0300

    Refactor textare field control

--- src/components/controls/fields/TextareaField.vue
+++ src/components/controls/fields/TextareaField.vue
@@ -2,7 +2,7 @@
     <div class="textarea-field" v-if="newField">
 
         <label>{{newField.label}}</label>
-        <textarea v-model="value" @input="$emit('change', newField.name, newField.value, idx)"
+        <textarea v-model="newField.value" @input="update"
             :rows="newField.rows" :placeholder="newField.placeholder" />
 
     </div>
@@ -14,24 +14,22 @@ export default {
     props: [ 'field', 'idx' ],
     data() {
         return {
-            newField: JSON.parse(JSON.stringify(this.field))
+            newField: null
         }
     },
-    computed: {
-        value: {
-            get() {
-                return this.newField.value || this.newField.default || '';
-            },
-            set(newVal) {
-                this.newField.value = newVal;
-            }
+    methods: {
+        update() {
+            this.$emit('change', this.newField.name, this.newField.value, this.idx);
         }
     },
     watch: {
         field: {
-            deep: true,
+            immediate: true,
             handler() {
-                this.newField = JSON.parse(JSON.stringify(this.field))
+                this.newField = JSON.parse(JSON.stringify(this.field));
+                this.newField.value = this.newField.value || this.newField.default || '';
+                this.newField.rows = this.newField.rows || 3;
+                this.newField.placeholder = this.newField.placeholder || '';
             }
         }
     }
