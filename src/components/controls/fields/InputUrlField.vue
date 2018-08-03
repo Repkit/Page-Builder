129ed53f208c017ad7fba2c602b20185d265e324
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-08-03 12:05:10 +0300

    Refactor url field control

--- src/components/controls/fields/InputUrlField.vue
+++ src/components/controls/fields/InputUrlField.vue
@@ -2,7 +2,7 @@
     <div class="input-url-field" v-if="newField">
 
         <label>{{newField.label}}</label>
-        <input v-model="value" @input="$emit('change', newField.name, newField.value, idx)"
+        <input v-model="newField.value" @input="update"
             type="url" name="newField.name" :placeholder="newField.placeholder" />
 
     </div>
@@ -14,24 +14,21 @@ export default {
     props: [ 'field', 'idx' ],
     data() {
         return {
-            newField: JSON.parse(JSON.stringify(this.field))
+            newField: null
         }
     },
-    computed: {
-        value: {
-            get() {
-                return this.newField.value || this.newField.default || '';
-            },
-            set(newVal) {
-                this.newField.value = newVal;
-            }
+    methods: {
+        update() {
+            this.$emit('change', this.newField.name, this.newField.value, this.idx);
         }
     },
     watch: {
         field: {
-            deep: true,
+            immediate: true,
             handler() {
-                this.newField = JSON.parse(JSON.stringify(this.field))
+                this.newField = JSON.parse(JSON.stringify(this.field));
+                this.newField.value = this.newField.value || this.newField.default || '';
+                this.newField.placeholder = this.newField.placeholder || '';
             }
         }
     },
